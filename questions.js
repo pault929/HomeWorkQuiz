24fe8397b3682bc8dc8cc8cf20e4d4f30b6ff818
Paul T <paultoro101@gmail.com>
2020-09-11 14:26:57 -0400

    added files to HoweworkQuiz

---
+++ questions.js
@@ -0,0 +1,50 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      tittle: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      tittle: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      tittle:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      tittle:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
+  
+  var score = 0;
+  
+  for (var i= 0; i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+      score++;
+      alert("Correct");
+    }else{
+      alert ("Wrong!");
+    }
+   }
+   alert("you got " + score + "/" + questions.length);
+  
+  
